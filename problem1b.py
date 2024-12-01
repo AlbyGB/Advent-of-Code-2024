24d7e55287c43f9562a9f10719d50dbe7abe77f2
albyg <alby0705@gmail.com>
2024-12-01 16:45:31 +0100

    primo giorno

---
+++ problem1b.py
@@ -0,0 +1,25 @@
+import re
+
+f_list = []
+s_list = []
+
+regex = r'\b\d+\b'
+
+somma = 0
+
+with open('inputs/problem1input.txt', 'r') as file:
+    for riga in file:
+        numeri = list(map(int, re.findall(regex, riga)))
+        f_list.append(numeri[0])
+        s_list.append(numeri[1])
+
+    f_list.sort()
+    s_list.sort()
+
+    for i, el in enumerate(f_list):
+        occ = s_list.count(el)
+
+        somma += el*occ
+
+
+print(somma)
